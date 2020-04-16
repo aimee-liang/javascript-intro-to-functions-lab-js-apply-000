956e830e9f1e8265c9e73f330fc13683c65f5d54
Aimee Liang <aimeeeliang@gmail.com>
2020-04-16 18:45:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ function shout(string){
   return string.toUpperCase()
 }
 
+/* Exercise 2:  describe('whisper(string)', function() {
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(whisper('HELLO')).toEqual('hello')
+  })
+})
+*/
+function whisper(string){
+  return string.toLowerCase()
+}
+/* Exercise 3: 
+*/
